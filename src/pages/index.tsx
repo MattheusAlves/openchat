e7b9c547f752b7b39441738f662ab204f97bdf1b
MattheusAlves <matheus_alves0101@outlook.com>
2021-04-04 14:37:12 -0300

    feat: init index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,12 @@
+import styles from '@/styles/index.module.scss'
+
 const Auth = (): JSX.Element => {
-  return <div></div>
+  return (
+    <div className={styles.container}>
+      <div className={styles['auth-wrapper']}></div>
+      <div className={styles.separator}></div>
+    </div>
+  )
 }
 
 export default Auth

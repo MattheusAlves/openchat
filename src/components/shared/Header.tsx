a260e78e5c3194bf1868a7a864299070e6db0d2d
MattheusAlves <matheus_alves0101@outlook.com>
2021-05-25 13:49:16 -0300

    directory changes

--- src/components/shared/Header.tsx
+++
@@ -1,9 +0,0 @@
-const Header = (): JSX.Element => {
-  return (
-    <div className="header-container">
-      <span className="header-title">OpenChat</span>
-    </div>
-  )
-}
-
-export default Header
